ae2cd2e65a61f1f39111e0efe889ea6a3510e80c
John Hardy <jh@lagado.com>
2017-11-23 01:02:24 +1100

    adding redux to startere

---
+++ src/app/middlewares.js
@@ -0,0 +1,12 @@
+import angular from 'angular';
+
+export const middlewares = angular
+  .module('middlewares', [])
+  .factory('middleware1', ($log) => {
+    'ngInject';
+
+    return store => next => (action) => {
+      $log.log('middleware', store, next, action);
+    };
+  })
+  .name;
--- src/app/root.component.js
+++ src/app/root.component.js
@@ -2,4 +2,18 @@ import templateUrl from './root.html';
 
 export const rootComponent = {
   templateUrl,
+  controller: class ContactsComponent {
+    constructor($ngRedux, $scope) {
+      'ngInject';
+
+      const unsubscribe = $ngRedux.connect(this.mapStateToThis)(this);
+      $scope.$on('$destroy', unsubscribe);
+    }
+
+    mapStateToThis(state) {
+      return {
+        x: state.x,
+      };
+    }
+  },
 };
