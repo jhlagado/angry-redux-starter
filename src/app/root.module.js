132ea1b21f8926d7ae07b0c4c795a30cf509ddb0
John Hardy <jh@lagado.com>
2017-11-23 01:02:24 +1100

    adding redux to startere

--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,24 +1,49 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
+import ngRedux from 'ng-redux';
+
 import { rootComponent } from './root.component';
 import { common } from './common/common.module';
 import { components } from './components/components.module';
 import './root.scss';
 
 import registerServiceWorker from './registerServiceWorker';
+import { middlewares } from './middlewares';
 
 registerServiceWorker();
 
 export const root = angular
   .module('root', [
     uiRouter,
+    ngRedux,
     common,
     components,
+    middlewares,
   ])
   .component('root', rootComponent)
   .config(($locationProvider) => {
     'ngInject';
 
     $locationProvider.html5Mode(true);
+  })
+  .config(($ngReduxProvider) => {
+    'ngInject';
+
+    const rootReducer = (state = { x: 123 }) => state;
+
+    $ngReduxProvider.createStoreWith(
+      rootReducer,
+      ['middleware1'],
+      /* eslint-disable no-underscore-dangle */
+      [window.__REDUX_DEVTOOLS_EXTENSION__()]
+      /* eslint-enable */
+    );
+  })
+  .run(($ngRedux, $rootScope, $timeout) => {
+    // To reflect state changes when disabling/enabling actions via the monitor
+    // there is probably a smarter way to achieve that
+    $ngRedux.subscribe(() => {
+      $timeout(() => { $rootScope.$apply(() => { }); }, 100);
+    });
   })
   .name;
